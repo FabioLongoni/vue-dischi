0c2c55e16f26a16a9de512665f261dd38d040618
FabioLongoni <longoni.fabio90@gmail.com>
2022-09-07 15:54:04 +0200

    add axios-add MainComponent

---
+++ src/components/MainComponent.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container"> 
+    <div v-for="disco,i in discoList" :key="i">
+      {{disco}}
+    </div>   
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'MainComponent',
+  data() {
+    return {
+      discoList: [],
+    }
+  },
+  created() {
+    axios
+      .get('https://flynn.boolean.careers/exercises/api/array/music')
+      .then((res) => {
+        this.discoList = res.data;
+        console.log(res.data);
+      });
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
